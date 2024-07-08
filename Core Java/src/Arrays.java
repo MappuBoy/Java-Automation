34601bf36a8301b4198bd213ea36957ad7c45896
mappuboy <pituvaraliyange@gmail.com>
2024-07-08 20:10:13 +0530

    simple arrays initializing and declaring

---
+++ Core Java/src/Arrays.java
@@ -0,0 +1,21 @@
+public class Arrays {
+    public static void main(String[] args) {
+
+        //Arrays can create two different ways
+
+        // 1st way
+        int[] array = new int[5]; //declaring the value
+        array[0] = 1; //values assigning from the "0" index //initialize the array
+        array[1] = 1;
+        array[2] = 1;
+        array[3] = 1;
+        array[4] = 1;
+
+       //2nd way
+        int[] arraytwo = {1,2,3,4,5,6};
+
+        System.out.println("Array index of the zero " + array[0]);
+
+
+    }
+}
