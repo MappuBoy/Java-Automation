924ad3343192621c0305966cf6b2cb83839c9299
mappuboy <pituvaraliyange@gmail.com>
2024-07-08 19:57:13 +0530

    data types and string concatenation

--- Core Java/src/Main.java
+++ Core Java/src/Main.java
@@ -2,14 +2,29 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+
+        //variables and data types
+
+        //primitive data type
+        int number_one = 5;
+        System.out.println(number_one);
+        //single charecter
+        char letter = 'r';
+        //decimal numbers
+        float decimal = 1.21F;
+        double big_double = 12.234;
+
+        //Non-primitive data types (collection of characters)
+        String website = "Navod Amarasinghe";
+        System.out.println(website);
+
+        //String concatenation (using "+" is using to concatenate the numbers and strings)
+        System.out.println(decimal+"floating point value");
+        System.out.println(big_double+"floating point value that have lots of numbers");
+        System.out.println(letter+"char values use to display the single characters and it have numarical values");
+        System.out.println(decimal+"floating point value");
+        System.out.println("decimal+\"floating point value\"");
+
     }
 }
