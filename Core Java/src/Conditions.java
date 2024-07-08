6a33caf3ebf1a5ce580b2a3cdd6b08c3fc76673c
mappuboy <pituvaraliyange@gmail.com>
2024-07-08 20:36:20 +0530

    If statement

--- Core Java/src/Conditions.java
+++ Core Java/src/Conditions.java
@@ -1,2 +1,14 @@
 public class Conditions {
+    public static void main(String[] args) {
+
+        int [] array = {1,2,3,4,5,6,7,8,9,0};
+        //print 2,4,6,8,0
+
+        //if else block
+        for (int i =0; i< array.length; i++){
+            if (array [i] %2 == 0){ // use to get the "%" reminder and use "==" to equaly compare
+                System.out.println(array[i]);
+            }
+        }
+    }
 }
